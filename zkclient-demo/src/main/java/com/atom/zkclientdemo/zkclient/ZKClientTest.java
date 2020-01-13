d8055200f601916e92e06c6edbad924e38ae14ee
liuyaming <sarming@126.com>
2020-01-13 18:10:54 +0800

    zkclient创建临时节点

--- zkclient-demo/src/main/java/com/atom/zkclientdemo/zkclient/ZKClientTest.java
+++ zkclient-demo/src/main/java/com/atom/zkclientdemo/zkclient/ZKClientTest.java
@@ -5,8 +5,6 @@ import org.I0Itec.zkclient.ZkConnection;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -29,6 +27,18 @@ public class ZKClientTest {
     @Test
     public void testRecursiveCreate() {
         zkClient.createPersistent("/temp/a/b/c", true);
+        assertThat(zkClient.exists("/temp/a/b/c"), equalTo(true));
+        final boolean b = zkClient.deleteRecursive("/temp/a/b/c");
+        assertThat(b, equalTo(true));
     }
 
+    /**
+     * zkclient创建临时节点
+     */
+    @Test
+    public void testCreateEphemeral() {
+        zkClient.createEphemeral("/testephemeral", "aaa");
+        assertThat(zkClient.exists("/testephemeral"), equalTo(true));
+        zkClient.delete("/testephemeral");
+    }
 }
