e124c74cd20473d73fa518c9f0b80b2bde786a96
liuyaming <sarming@126.com>
2020-01-13 17:00:58 +0800

    zkclient

---
+++ zkclient-demo/src/test/java/com/atom/zkclientdemo/ZkclientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atom.zkclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkclientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
